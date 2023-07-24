558ee5b2faf3b7b7b7a34a58d98ab935b2ad4155
AryamanatNEC <140397829+AryamanatNEC@users.noreply.github.com>
2023-07-24 20:44:31 +0530

    added scrapy crawler

--- shopping_spider1/shopping_spider/spiders/shopping_spider.py
+++ shopping_spider1/shopping_spider/spiders/shopping_spider.py
@@ -1,64 +1,74 @@
 import scrapy
-from bs4 import BeautifulSoup
+import json
 
-def extract_number_from_string(s):
-    # Helper function to extract numeric part from a string
-    return float("".join(filter(str.isdigit, s)))
+class GoogleShoppingSpider(scrapy.Spider):
+    name = 'google_shopping'
+    allowed_domains = ['google.com']
 
-class ShoppingSpider(scrapy.Spider):
-    name = 'shopping_spider'
-    start_urls = ['https://www.google.com/search?q=nike+shoes&tbm=shop&gl=us']
+    def __init__(self, search_query=None, *args, **kwargs):
+        super(GoogleShoppingSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [f'https://www.google.com/search?q={self.format_search_query(search_query)}&tbm=shop&gl=ind']
+
+    def format_search_query(self, search_query):
+        formatted_query = search_query.replace("'", '').lower().replace('"', '').replace(' ', '+')
+        return formatted_query
 
     def parse(self, response):
-        soup = BeautifulSoup(response.text, "html.parser")
+        ads_data = []
+        shopping_results_data = []
 
-        ads = []
-        for el in soup.select(".sh-np__click-target"):
+        for el in response.css(".sh-np__click-target"):
             ad = {
-                "title": el.select_one(".sh-np__product-title").get_text() if el.select_one(".sh-np__product-title") else None,
-                "link": "https://google.com" + el.get("href"),
-                "source": el.select_one(".sh-np__seller-container").get_text() if el.select_one(".sh-np__seller-container") else None,
-                "price": el.select_one(".hn9kf").get_text() if el.select_one(".hn9kf") else None,
-                "delivery": el.select_one(".U6puSd").get_text() if el.select_one(".U6puSd") else None,
+                "title": el.css(".sh-np__product-title::text").get(),
+                "link": "https://google.com" + el.css("::attr(href)").get(),
+                "source": el.css(".sh-np__seller-container::text").get(),
+                "price": el.css(".hn9kf::text").get(),
+                "delivery": el.css(".U6puSd::text").get(),
             }
-            extensions = el.select_one(".rz2LD")
+            extensions = el.css(".rz2LD::text").get()
             if extensions:
-                ad["extensions"] = extensions.get_text()
-            ads.append(ad)
+                ad["extensions"] = extensions
+            ads_data.append(ad)
 
-        for ad in ads:
-            ad.pop("", None)
-
-        shopping_results = []
-        for el in soup.select(".sh-dgr__gr-auto"):
+        for el in response.css(".sh-dgr__gr-auto"):
             result = {
-                "title": el.select_one("h3.tAxDx").get_text() if el.select_one("h3.tAxDx") else None,
-                "link": el.select_one(".zLPF4b .eaGTj a.shntl")["href"][el.select_one("a.shntl")["href"].index("=") + 1:] if el.select_one(".zLPF4b .eaGTj a.shntl") else None,
-                "source": el.select_one(".IuHnof").get_text().replace(".aULzUe{.*?}.aULzUe::after{.*?}", "") if el.select_one(".IuHnof") else None,
-                "price": el.select_one(".XrAfOe .a8Pemb").get_text() if el.select_one(".XrAfOe .a8Pemb") else None,
+                "title": el.css("h3.tAxDx::text").get(),
+                "link": el.css(".zLPF4b .eaGTj a.shntl::attr(href)").get()[el.css("a.shntl::attr(href)").get().index("=") + 1:],
+                "source": el.css(".IuHnof::text").get().replace(".aULzUe{.*?}.aULzUe::after{.*?}", "") if el.css(".IuHnof::text").get() else None,
+                "price": el.css(".XrAfOe .a8Pemb::text").get(),
                 "rating": None,
                 "reviews": None,
-                "delivery": el.select_one(".vEjMR").get_text() if el.select_one(".vEjMR") else None,
+                "delivery": el.css(".vEjMR::text").get(),
             }
-            rating_element = el.select_one(".NzUzee .QIrs8")
+
+            rating_element = el.css(".NzUzee .QIrs8")
             if rating_element:
-                rating_text = rating_element.get_text().strip()
+                rating_text = rating_element.css("::text").get().strip()
                 if "out" in rating_text:
                     try:
                         result["rating"] = float(rating_text.split("out")[0].strip())
-                        result["reviews"] = extract_number_from_string(rating_text)
+                        result["reviews"] = self.extract_number_from_string(rating_text)
                     except ValueError:
                         pass
 
-            extensions = el.select_one(".Ib8pOd")
+            extensions = el.css(".Ib8pOd::text").get()
             if extensions:
-                result["extensions"] = extensions.get_text()
-            shopping_results.append(result)
+                result["extensions"] = extensions
+            shopping_results_data.append(result)
 
-        for result in shopping_results:
+        for ad in ads_data:
+            ad.pop("", None)
+
+        for result in shopping_results_data:
             result.pop("", None)
 
         yield {
-            "ads": ads,
-            "shopping_results": shopping_results
+            "ads_data": ads_data,
+            "shopping_results_data": shopping_results_data
         }
+
+    def extract_number_from_string(self, s):
+        parts = s.split()
+        for i in range(len(parts)):
+            if parts[i] == 'stars.':
+                return parts[i+1]
